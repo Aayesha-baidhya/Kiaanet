999e505034bc209ef27b86fe700997a0087f8ec0
Aayesha Baidhya <aayeshabaidhya@gmail.com>
2025-08-07 12:54:11 +0545

    feat:  renamed about-me to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,16 +3,16 @@ import Announcement from '@/components/announcement'
 import LoginForm from '@/components/loginForm'
 import React from 'react'
 
-export default function about() {
+export default function page() {
   return (
-    <div className="min-h-screen flex items-stretch">
+    <div className="min-h-screen flex flex-col md:flex-row">
 
         <div className="w-full md:w-1/2 bg-[#8CA0ED] flex items-center">
             {/* <h2>About</h2> */}
             {/* <PrimaryBtn/> Calling the component */}
             <Announcement/>
             </div>
-        <div className="w-full md:w-1/2 flex items-center justify-center bg-white">
+        <div className="w-full md:w-1/2 mt-5 flex items-center justify-center bg-white">
             <LoginForm/>
             </div>
     </div>
