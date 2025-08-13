2569df2a28e7d0dfd44dbce7d697f312c279da9c
Aayesha Baidhya <aayeshabaidhya@gmail.com>
2025-08-13 18:30:18 +0545

    feat: sidebar and navbar reusability

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from "@/components/navBar"
+import SideBar from "@/components/sideBar"
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <section>
+    <div className="flex h-screen overflow-hidden">
+        <SideBar/>
+    <div className="flex flex-col flex-1">
+        <Navbar/>
+    {children}
+    </div>
+    </div>
+    </section>
+}
