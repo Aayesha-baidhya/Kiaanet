18f2a8982f3f3326d9d229c517019a3593b24d7d
Aayesha Baidhya <aayeshabaidhya@gmail.com>
2025-08-14 12:59:39 +0545

    feat: changed in content

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,18 +1,20 @@
-import Navbar from "@/components/navBar"
-import SideBar from "@/components/sideBar"
+import Navbar from "@/components/navBar";
+import SideBar from "@/components/sideBar";
 
 export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return <section>
-    <div className="flex h-screen overflow-hidden">
-        <SideBar/>
-    <div className="flex flex-col flex-1">
-        <Navbar/>
-    {children}
-    </div>
-    </div>
+  return (
+    <section>
+      <div className="flex h-screen overflow-hidden">
+        <SideBar />
+        <div className="flex flex-col flex-1">
+          <Navbar />
+          {children}
+        </div>
+      </div>
     </section>
+  );
 }
