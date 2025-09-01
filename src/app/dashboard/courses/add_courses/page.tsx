f2304ac414c3f356c5a6ec84c991cdc1599e369f
Aayesha Baidhya <aayeshabaidhya@gmail.com>
2025-09-01 14:09:30 +0545

    feat:Integrating add courses

---
+++ src/app/dashboard/courses/add_courses/page.tsx
@@ -0,0 +1,10 @@
+import AddCourses from '@/components/addCourses';
+import React from 'react'
+
+const AddCoursesPage = () => {
+  return (
+    <AddCourses/>
+  )
+}
+
+export default AddCoursesPage;
--- src/app/dashboard/courses/page.tsx
+++ src/app/dashboard/courses/page.tsx
@@ -1,44 +1,46 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { CiSquarePlus } from "react-icons/ci";
 
-const page = () => {
-    const listOfCourse = [
-        {id: 1,
-        name: "IOT",
-        description: "This is IOT"
-        },
-        {id: 2,
-        name: "Database",
-        description: "This is Database"
-        },
-        {id: 3,
-        name: "Computing",
-        description: "This is fundamentals of computing"
-        }
-    ]
+const Page = () => {
+  const listOfCourse = [
+    { id: 1, name: "IOT", description: "This is IOT" },
+    { id: 2, name: "Database", description: "This is Database" },
+    {
+      id: 3,
+      name: "Computing",
+      description: "This is fundamentals of computing",
+    },
+  ];
+
   return (
     <>
-    <div>These are the available courses.</div>
-    <h1>List of Courses</h1>
-    {
-        listOfCourse.map((item)=>{
-            return(
-                <div className="py-4" key={item.id}>
-                <div>
-                    <Link href={`/dashboard/courses/${item.name}`}>
-                    {item.name}
-                    </Link>
-                </div>
-                <div>
-                    {item.description}
-                </div>
-                </div>
-            )
-        })
-    }
+      <div className="flex items-center justify-between my-4">
+        <div>
+          <div>These are the available courses.</div>
+          <h1 className="text-xl font-bold">List of Courses</h1>
+        </div>
 
+        <Link
+          href="/dashboard/courses/add_courses"
+          className="bg-blue-600 hover:bg-blue-700 text-white px-3 py-2 rounded-lg text-sm flex items-center gap-2 inline-flex"
+        >
+          <CiSquarePlus className="text-white text-lg" />
+          Add Courses
+        </Link>
+      </div>
+
+      {/* Courses list */}
+      {listOfCourse.map((item) => (
+        <div className="py-4" key={item.id}>
+          <div>
+            <Link href={`/dashboard/courses/${item.name}`}>{item.name}</Link>
+          </div>
+          <div>{item.description}</div>
+        </div>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
